6308b25052023b2776a353012b41f9c81749afb5
Danilo <daniloaugusto0212@hotmail.com>
2021-03-26 20:26:06 -0300

    Eventos de carregamento com jQuery

---
+++ javascript-danki/jQuery/aula01/js/functions.js
@@ -0,0 +1,16 @@
+// $(function(){
+//     alert('Olá');
+// })
+
+// $(document).read(function(){
+//     alert('Hello')
+// })
+
+// $(window).on('load', function(){
+//     alert('Olá')
+// })
+//O código funciona normalmente das 3 maneiras
+
+$(function(){
+    alert('Olá');
+})
