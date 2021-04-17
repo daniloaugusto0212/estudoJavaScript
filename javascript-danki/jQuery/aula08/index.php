b2dee53b6542eaa1343d8fb984a55b43de39fdaf
Danilo <daniloaugusto0212@hotmail.com>
2021-04-17 19:39:20 -0300

    Funções para adiciona e remover elemento c/ jQuery

---
+++ javascript-danki/jQuery/aula08/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="box">
+        <h1>Meu título</h1>
+        <p>Meu conteúdo</p>
+        <div>Outro conteúdo</div>
+    </div>
+
+    <div class="box">
+        <h1>Meu título</h1>
+        <p>Meu conteúdo</p>
+    </div>
+
+    <script src="../js/jquery-3.6.js"></script>
+    <script src="functions.js"></script>
+</body>
+</html>
