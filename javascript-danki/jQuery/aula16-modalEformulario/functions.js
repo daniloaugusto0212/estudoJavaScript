50ffc521e45b21f7c9532b6cb81e7e22d26249f5
Danilo <daniloaugusto0212@hotmail.com>
2021-08-05 22:54:34 -0300

    Expressões regulares

--- javascript-danki/jQuery/aula16-modalEformulario/functions.js
+++ javascript-danki/jQuery/aula16-modalEformulario/functions.js
@@ -22,6 +22,37 @@ $(function(){
         })
     }
 
+    $('form#form1').submit(function(){
+        var nome = $('input[name=nome]').val()
+        var telefone = $('input[name=telefone]').val()
+        var email = $('input[name=email]').val()
+
+        //verificar se tem pelo menos 2 nomes
+        var amount = nome.split(' ').length >= 2
+        var splitStr = nome.split(' ')
+        if (amount) {
+            for (var i = 0; i < amount; i++) {
+                if (splitStr[i].match(/^[A-Z]{1}[a-z]{1,}$/)) {
+                    console.log('Nossa condição bateu podemos dar continuidade')
+                } else {
+                    aplicarCampoInvalido($('input[name=nome]'))
+                    return false
+                }
+            }
+        } else {
+            $('input[name=nome]').css('border', '2px solid red')
+            $('input[name=nome]').data('invalido', true)
+            return false
+        }
+
+    })
+
+    function aplicarCampoInvalido(el) {
+        el.css('border', '2px solid red')
+        el.val('Campo Inválido!')
+        el.data('invalido', true)
+    }
+
 });
 
 
