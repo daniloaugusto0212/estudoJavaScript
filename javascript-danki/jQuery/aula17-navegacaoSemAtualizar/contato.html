285131f24ab7ebfdad660287d6f9caab451b32e2
Danilo <daniloaugusto0212@hotmail.com>
2021-08-12 21:33:17 -0300

    Atualizando página sem recarregar

---
+++ javascript-danki/jQuery/aula17-navegacaoSemAtualizar/contato.html
@@ -0,0 +1,1 @@
+<h2>Página contato</h2>
---
+++ javascript-danki/jQuery/aula17-navegacaoSemAtualizar/functions.js
@@ -0,0 +1,31 @@
+$(function () {
+
+    verficarCliqueMenu();
+
+    function verficarCliqueMenu() {
+        $('a').click(function () {
+            var href = $(this).attr('href');
+            $.ajax({
+                'beforeSend': function () {
+                    console.log('Estamos chamando o Before Send');
+                },
+                'timeout': 10000,
+                'url': href,
+                'error': function (jqXHR, textStatus, errorThrown) {
+                    if (jqXHR.statusText == 'Not Found') {
+                        console.log("Página não encontrada")
+                    }
+                },
+                'success': function (data) {
+                    //$('#container').html(data)
+                    $(data).appendTo('#container').fadeIn();
+                }
+            })
+            return false;
+        })
+    }
+});
+
+
+
+
